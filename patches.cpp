ed808c6f470dc005483cd14b32ad7ce91fc1ca11
xiBryan <91988239+xiBryanDev@users.noreply.github.com>
2023-08-11 16:54:53 -0300

    Update patches.cpp

--- patches.cpp
+++ patches.cpp
@@ -5,7 +5,7 @@ void Com_Error(int code, const char* fmt, char * ap)
 	Com_Sprintf(buffer, sizeof(buffer), fmt, ap);
 	if (strstr(buffer, "2echoreply"))
 	{
-		UI_OpenToastPopup(0, "hud_obit_death_suicide", "Kick bypassed", "Kick bypassed", 19000);//RME
+		UI_OpenToastPopup(0, "hud_obit_death_suicide", "Kick bypassed", "Kick bypassed", 19000);
 		return;
 	}
 	Com_Error_t->invoke(code, fmt, ap);
