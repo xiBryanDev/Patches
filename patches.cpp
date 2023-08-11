58932d08e2c5d1dbd67644e45ed4d75c57785f37
xiBryan <91988239+xiBryanDev@users.noreply.github.com>
2023-08-11 16:53:35 -0300

    patches for Kick Nexus

---
+++ patches.cpp
@@ -0,0 +1,14 @@
+detour* Com_Error_t;
+void Com_Error(int code, const char* fmt, char * ap)
+{
+	char buffer[400];
+	Com_Sprintf(buffer, sizeof(buffer), fmt, ap);
+	if (strstr(buffer, "2echoreply"))
+	{
+		UI_OpenToastPopup(0, "hud_obit_death_suicide", "Kick bypassed", "Kick bypassed", 19000);//RME
+		return;
+	}
+	Com_Error_t->invoke(code, fmt, ap);
+}
+
+Com_Error_t = new detour(0x31EE80, Com_Error);
